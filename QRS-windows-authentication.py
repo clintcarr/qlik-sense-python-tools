62f287523f7de38a75c0d87cc42d0f9293f5d535
Clint Carr <clint.carr@outlook.com>
2017-01-25 13:46:25 +1100

    Update QRS-windows-authentication.py

--- QRS-windows-authentication.py
+++ QRS-windows-authentication.py
@@ -8,10 +8,15 @@ headers = {"X-Qlik-XrfKey": "abcdefg123456789",
             "Content-Type": "application/json",
             "Connection": "Keep-Alive"}
 
+#establish a session (NTLM challenges on each request, performing all queries in a session prevents this)
 session = requests.Session()
+#set the session authentication to HttpNtlmAuth, attach it to the session
 session.auth = HttpNtlmAuth('qliklocal\\administrator','Qlik1234', session)
+#connect to the API qrs/about using Windows Authentication in the session
 x = session.get('https://qs2.qliklocal.net/qrs/about?xrfkey=abcdefg123456789', verify=False, headers = headers)
+#return the URL (this is the redirect from Qlik Sense https://qs2.qliklocal.net/form/?targetid{}
 url = x.url
+#replace the page form with windows_authentication and hit the url again
 y = session.get(url.replace('form', 'windows_authentication'), verify=False, headers = headers)
 
 print (y.content)
